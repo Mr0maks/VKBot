9d981631e3899135bd29dd7b0c5d202e56aedede
Mr0maks <mr.maks0443@gmail.com>
2019-10-19 18:55:20 +0500

    End of antivoice

--- modules/core/src/events.c
+++ modules/core/src/events.c
@@ -8,6 +8,7 @@ int message_new_hook(cJSON *raw)
 {
     cJSON *object = cJSON_GetObjectItem(raw, "object");
     cJSON *peer_id = cJSON_GetObjectItem(object, "peer_id");
+    cJSON *from_id = cJSON_GetObjectItem(object, "from_id");
     cJSON *attachments = cJSON_GetObjectItem(object, "attachments");
 
     cJSON *attach = NULL;
@@ -15,13 +16,16 @@ int message_new_hook(cJSON *raw)
     if(!attachments)
         return MODULE_IGNORE;
 
+    if(!cJSON_GetArraySize(attachments))
+        return MODULE_IGNORE;
+
     cJSON_ArrayForEach(attach, attachments)
     {
         if(!strcmp("audio_message", cJSON_GetObjectItem(attach, "type")->valuestring))
         {
             string_t message_format = STRING_INIT();
-            STRING_FORMAT(message_format, "text=\"Argv[1] ненужон. Войсер обнаружен!\"&&forward_messages=%i&&peer_id=%i", cJSON_GetObjectItem(raw, "conversation_message_id")->valueint, peer_id->valueint );
-            VKAPI_CALL_METHOD("message.send", message_format, false);
+            STRING_FORMAT(message_format, "random_id=0&message=argv[1] ненужон. [id%i|Войсер] обнаружен!\n&forward_messages=%i&peer_id=%i", from_id->valueint, cJSON_GetObjectItem(object, "conversation_message_id")->valueint, peer_id->valueint );
+            VKAPI_CALL_METHOD("messages.send", message_format, false);
             STRING_DESTROY(message_format);
             return MODULE_OVERRIDE;
         }
