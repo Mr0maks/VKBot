f8b9f6331762a24f9d2cef126451096468f0aa65
Mr0maks <mr.maks0443@gmail.com>
2020-04-25 13:30:39 +0500

    num_threads = 4

--- heroku_run.sh
+++ heroku_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo "token = "${VK_TOKEN} >> release.linux.x86_64/bot.ini
+echo "token = "${VK_TOKEN} > release.linux.x86_64/bot.ini
+echo "num_workers = 8" >> release.linux.x86_64/bot.ini
 cd release.linux.x86_64/
 ./vkbot_x86_64
