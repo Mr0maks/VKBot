1c0d6411a1fab4976beb74d36ba2e74db51ccc65
Mr0maks <mr.maks0443@gmail.com>
2020-06-11 23:36:07 +0500

    chat_kick_user

--- modules/core/src/events.c
+++ modules/core/src/events.c
@@ -49,5 +49,20 @@ bool chat_invite_user_handler(cJSON *raw)
 
 bool chat_kick_user(cJSON *raw)
 {
+    cJSON *peer_id = cJSON_GetObjectItem(cJSON_GetObjectItem(raw, "object"), "peer_id");
+    cJSON *from_id = cJSON_GetObjectItem(cJSON_GetObjectItem(raw, "object"), "from_id");
+
+    if(!from_id)
+        return false;
+
+    if(!peer_id)
+        return false;
+
+    if(from_id->valueint < 0)
+    {
+        VKAPI_SEND_MESSAGE(peer_id->valueint, "Пока бот :)", NULL, 0 );
+    } else {
+        VKAPI_SEND_MESSAGE(peer_id->valueint, "Этот парень был из тех, кто просто любит жизнь.", NULL, 0 );
+    }
     return false;
 }
