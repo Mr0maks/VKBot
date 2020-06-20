7d8174f1af90addcf2dc795f0d1fa68629178f1e
Mr0maks <mr.maks0443@gmail.com>
2020-06-20 21:25:07 +0500

    set 4 workers per 1 cpu for heroku + set CFLAGS=-O2

--- heroku_run.sh
+++ heroku_run.sh
@@ -4,6 +4,6 @@ echo "openssl" >> release.linux.x86_64/modules.ini
 
 echo "token = "${VK_TOKEN} > release.linux.x86_64/bot.ini
 echo "debug = true" >> release.linux.x86_64/bot.ini
-echo "num_workers = 8" >> release.linux.x86_64/bot.ini
+echo "num_workers = 32" >> release.linux.x86_64/bot.ini
 cd release.linux.x86_64/
 ./vkbot_x86_64
