f63f0a28c4db9995e0111c950de39fe32c125240
Mr0maks <mr.maks0443@gmail.com>
2020-11-17 18:43:17 +0500

    fix bot checking

--- modules/core/src/events.c
+++ modules/core/src/events.c
@@ -2,6 +2,7 @@
 #include <enginecallbacks.h>
 #include <strings.h>
 
+#if 0
 int message_new_hook(cJSON *raw)
 {
     cJSON *object = cJSON_GetObjectItem(raw, "object");
@@ -13,26 +14,22 @@ int message_new_hook(cJSON *raw)
     if(!from_id || !peer_id || !text)
         return false;
 
-    if(0 > from_id->valueint && !strcasecmp(cJSON_GetStringValue(text), "саксбот"))
-    {
-        VKAPI_SEND_MESSAGE(peer_id->valueint, "Привет бот >:(", NULL, 0 );
-        VKAPI_SEND_MESSAGE(peer_id->valueint, "максбот", NULL, 0 );
-    }
-
     return MODULE_IGNORE;
 }
+#endif
 
 int chat_invite_user_handler(cJSON *raw)
 {
     cJSON *object = cJSON_GetObjectItem(raw, "object");
     cJSON *message = cJSON_GetObjectItem(object, "message");
+    cJSON *action = cJSON_GetObjectItem(object, "action");
     cJSON *peer_id = cJSON_GetObjectItem(message, "peer_id");
-    cJSON *from_id = cJSON_GetObjectItem(message, "from_id");
+    cJSON *member_id = cJSON_GetObjectItem(action, "member_id");
 
-    if(!from_id || !peer_id)
+    if(!member_id || !peer_id)
         return false;
 
-    if(from_id->valueint < 0)
+    if(member_id->valueint < 0)
     {
             VKAPI_SEND_MESSAGE(peer_id->valueint, "Привет бот >:(", NULL, 0 );
     } else {
@@ -45,17 +42,18 @@ int chat_kick_user(cJSON *raw)
 {
     cJSON *object = cJSON_GetObjectItem(raw, "object");
     cJSON *message = cJSON_GetObjectItem(object, "message");
+    cJSON *action = cJSON_GetObjectItem(object, "action");
     cJSON *peer_id = cJSON_GetObjectItem(message, "peer_id");
-    cJSON *from_id = cJSON_GetObjectItem(message, "from_id");
+    cJSON *member_id = cJSON_GetObjectItem(action, "member_id");
 
-    if(!from_id || !peer_id)
+    if(!member_id || !peer_id)
         return false;
 
-    if(from_id->valueint < 0)
+    if(member_id->valueint < 0)
     {
         VKAPI_SEND_MESSAGE(peer_id->valueint, "Пока бот :)", NULL, 0 );
     } else {
         VKAPI_SEND_MESSAGE(peer_id->valueint, "Этот парень был из тех, кто просто любит жизнь.", NULL, 0 );
     }
-    return false;
+    return true;
 }
--- modules/core/src/events.h
+++ modules/core/src/events.h
@@ -2,6 +2,6 @@
 
 #include <enginecallbacks.h>
 
-int message_new_hook(cJSON *raw);
+/*int message_new_hook(cJSON *raw);*/
 int chat_invite_user_handler(cJSON *raw);
 int chat_kick_user(cJSON *raw);
